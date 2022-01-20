ff24ae6c85f50747305f2e206955030188ceeefb
Jordy Tirado Torres <jordytiradotorres20@gmail.com>
2022-01-19 22:25:57 -0500

    fixed all

--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import DcScreen from '../components/dc/DcScreen';
 import HeroScreen from '../components/heroes/HeroScreen';
 import MarvelScreen from '../components/marvel/MarvelScreen';
